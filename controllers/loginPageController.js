fdfd32c6e5b290b9bf1a7619ec9be283b9b557ef
HWEEDONG YOO <yoojon675@gmail.com>
2024-05-15 16:43:18 -0700

    Complete login authentication

--- controllers/loginPageController.js
+++ controllers/loginPageController.js
@@ -1,4 +1,6 @@
-
+const User = require('../models/userModels');
+const bcrypt = require('bcrypt');
+const Joi = require('joi');
 
 const displayPage = async (req, res) => {
     try {
@@ -10,12 +12,40 @@ const displayPage = async (req, res) => {
 
 const authenticateUser = async (req, res) => {
     try {
-        const email = req.body.email; // Corrected to access 'email' field
-        const password = req.body.password; // Corrected to access 'password' field
+        const { password, email } = req.body;
 
-        // authentication logic here
+        // Validate request body
+        const schema = Joi.object({
+            password: Joi.string().max(255).required(),
+            email: Joi.string().email().max(255).required()
+        });
+
+        const validationResult = schema.validate({ password, email });
+        
+        if (validationResult.error) {
+            return res.status(400).send(validationResult.error.details[0].message);
+        }
+
+        // Find user by email
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        // Check if passwords match
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).send('Invalid email/password combination');
+        }
+        else {
+            // Authentication successful
+            res.send('Authentication successful');
+        }
     } catch (error) {
-        res.status(500).send(error);
+        console.error("Error in authenticateUser:", error);
+        res.status(500).send("Internal Server Error");
     }
 };
 
