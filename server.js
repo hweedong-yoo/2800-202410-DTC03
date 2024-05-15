7de22dd2cdaee45dfbc5965d7f512e8be719af8c
Erryl <errylgalang@gmail.com>
2024-05-15 00:37:06 -0700

    Add barebones server.js file

---
+++ server.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const mongoose = require('mongoose');
+const Joi = require("joi");
+const bcrypt = require('bcrypt');
+
+
+// Get environment variables
+const mongodbHost = process.env.MONGODB_HOST;
+const mongodbUser = process.env.MONGODB_USER;
+const mongodbPassword = process.env.MONGODB_PASSWORD;
+const mongodbDatabase = process.env.MONGODB_DATABASE;
+const mongodbSessionSecret = process.env.MONGODB_SESSION_SECRET;
+const nodeSessionSecret = process.env.NODE_SESSION_SECRET;
+
+const expireTime = 60 * 60 * 1000;
+
+// Create an instance of Express app
+const port = process.env.PORT || 3000;
+const app = express();
+
+// Use ejs view engine
+app.set("view engine", "ejs");
+
+// Serve static files from the "public" folder
+app.use(express.static('public'));
+
+// run main and catch any errors
+main().catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// Start the server
+async function main() {
+    await mongoose.connect(`mongodb+srv://${mongodbUser}:${mongodbPassword}@${mongodbHost}/?retryWrites=true&w=majority&appName=${mongodbDatabase}`);
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
