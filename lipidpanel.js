c4965b0b5482f2a699c2b447d4b4331c7d9411b3
HWEEDONG YOO <yoojon675@gmail.com>
2024-05-15 12:17:37 -0700

    Include triglycerides in lipid panel assessment

--- lipidpanel.js
+++ lipidpanel.js
@@ -1,6 +1,6 @@
 /* 
 LIPID PANEL:
-- Total Cholesterol:
+- Total Cholesterol: HDL + LDL + 20% triglycerides
   - Normal Range: Less than 200 mg/dL
     - High: Increased cardiovascular disease risk; lifestyle modifications or medications may be recommended.
 
@@ -9,15 +9,32 @@ LIPID PANEL:
     - High: Increased cardiovascular disease risk; lifestyle modifications or medications may be recommended.
 
 - HDL Cholesterol:
-  - Normal Range: 40 to 60 mg/dL
+  - Normal Range: Above 60 mg/dL
     - Low: Increased cardiovascular disease risk; lifestyle modifications or medications may be recommended.
+
+- Triglycerides:
+  - Normal Range: Less than 150 mg/dL
+    - Low: Increased cardiovascular disease risk; lifestyle modifications or medications may be recommended.
+
  */
 
+// Generate random data within a given range
+function generateData(min, max) {
+    const data = [];
+    for (let i = 1; i <= 30; i++) { // Data for 30 days
+        const value = Math.floor(Math.random() * (max - min + 1)) + min;
+        data.push(value);
+    }
+    return data;
+}
+
 // Data for Lipid Panel
-const totalCholesterolData = generateData(120, 240);
 const ldlCholesterolData = generateData(70, 150);
 const hdlCholesterolData = generateData(35, 65);
-
+const triCholesterolData = generateData(95, 155)
+const totalCholesterolData = ldlCholesterolData.map((num, index) => 
+    num + hdlCholesterolData[index] + triCholesterolData[index]*0.2
+);
 // Create ApexChart for Total Cholesterol
 const totalCholesterolApex = {
     chart: {
@@ -141,12 +158,56 @@ const hdlCholesterolApex = {
     colors: ['#6c757d']
 };
 
+// Create ApexChart for HDL Cholesterol
+const triCholesterolApex = {
+    chart: {
+        height: 350,
+        type: 'line',
+        zoom: {
+            enabled: false
+        },
+        toolbar: {
+            show: false
+        }
+    },
+    series: [{
+        name: 'Triglycerides',
+        data: triCholesterolData
+    }],
+    xaxis: {
+        categories: Array.from(Array(30), (_, i) => i + 1),
+        labels: {
+            show: false
+        }
+    },
+    yaxis: {
+    },
+    stroke: {
+        curve: 'straight',
+        width: 2
+    },
+    title: {
+        text: 'Triglycerides Levels',
+        align: 'left',
+        style: {
+            fontSize: '16px',
+            fontFamily: 'Orbitron',
+            fontWeight: 'bold',
+            color: '#334155'
+        }
+    },
+    colors: ['#23dd7d']
+};
+
 const totalCholesterolChart = new ApexCharts(document.querySelector("#total-cholesterol-chart"), totalCholesterolApex);
 const ldlCholesterolChart = new ApexCharts(document.querySelector("#ldl-cholesterol-chart"), ldlCholesterolApex);
 const hdlCholesterolChart = new ApexCharts(document.querySelector("#hdl-cholesterol-chart"), hdlCholesterolApex);
+const triCholesterolChart = new ApexCharts(document.querySelector("#tri-cholesterol-chart"), triCholesterolApex);
 totalCholesterolChart.render();
 ldlCholesterolChart.render();
 hdlCholesterolChart.render();
-document.getElementById('total-cholesterol-count').innerText = glucoseData[glucoseData.length - 1];
-document.getElementById('ldl-cholesterol-count').innerText = calciumData[calciumData.length - 1];
-document.getElementById('hdl-cholesterol-count').innerText = bunData[bunData.length - 1];
+triCholesterolChart.render();
+document.getElementById('total-cholesterol-count').innerText = totalCholesterolData[totalCholesterolData.length - 1];
+document.getElementById('ldl-cholesterol-count').innerText = ldlCholesterolData[ldlCholesterolData.length - 1];
+document.getElementById('hdl-cholesterol-count').innerText = hdlCholesterolData[hdlCholesterolData.length - 1];
+document.getElementById('tri-cholesterol-count').innerText = triCholesterolData[triCholesterolData.length - 1];
