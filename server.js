ce85118e93b9e5a0516ba2510e75d9884e87aa47
Erryl Galang <errylgalang@gmail.com>
2024-05-15 15:11:02 -0700

    Move 404 to last route

--- server.js
+++ server.js
@@ -30,16 +30,16 @@ app.use(express.static('public'));
 // Define routes
 const landingPageRoute = require('./routes/landingPage');
 const homePageRoute = require('./routes/home');
-const NotFoundController = require('./routes/404Page');
 const signupRoute = require('./routes/signupPage');
 const loginRoute = require('./routes/loginPage');
+const NotFoundController = require('./routes/404Page');
 
 // Use routes
 app.get('/', landingPageRoute);
 app.get('/home', homePageRoute);
-app.get('*', NotFoundController);
 app.get('/signup', signupRoute);
 app.get('/login', loginRoute);
+app.get('*', NotFoundController);
 
 // Start the server
 async function main() {
