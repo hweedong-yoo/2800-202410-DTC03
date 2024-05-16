f7c6e0bc5840428634d8c3d565979c5e40b8ebbd
MARIKO HOCKERTZ <mariko.hockertz@gmail.com>
2024-05-16 14:05:23 -0700

    Start editInformation function

--- controllers/editProfilePageController.js
+++ controllers/editProfilePageController.js
@@ -6,6 +6,16 @@ const displayPage = async (req, res) => {
   }
 };
 
+const editInformation = async (req, res) => {
+  try {
+
+
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
 module.exports = {
   displayPage,
+  editInformation,
 };
