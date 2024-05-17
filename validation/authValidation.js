12f6f69cb40b59195a071961ace9b86871cd734c
Erryl Galang <errylgalang@gmail.com>
2024-05-16 20:33:07 -0700

    Seperate JOI schema files

--- validation/authValidation.js
+++ validation/authValidation.js
@@ -1,24 +1,27 @@
+const { name } = require('ejs');
 const Joi = require('joi');
 
+const commonEmail = Joi.string().email().max(20).required();
+const commonPassword = Joi.string().max(20).required();
+const commonSecurityAnswer = Joi.string().max(20).required();
+
 const emailSchema = Joi.object({
-  email: Joi.string().email().max(20).required(),
+  email: commonEmail,
 });
 
 const securityAnswerSchema = Joi.object({
-  answer: Joi.string().max(20).required(),
+  answer: commonSecurityAnswer,
 });
 
 const passwordSchema = Joi.object({
-  password: Joi.string().max(20).required(),
+  password: commonPassword,
 });
 
-const signUpSchema = Joi.object(
-  {
-    name: securityAnswerSchema,
-    email: emailSchema,
-    password: passwordSchema,
+const signUpSchema = Joi.object({
+  name: commonSecurityAnswer,
+  email: commonEmail,
+  password: commonPassword,
 });
-
 
 module.exports = {
   emailSchema,
