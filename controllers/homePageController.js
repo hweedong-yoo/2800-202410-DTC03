5c17d1ac3c5bea29a5a78254a649358fb0dfb710
HWEEDONG YOO <yoojon675@gmail.com>
2024-05-23 07:33:19 -0700

    Implement fetching from blood collection for data on MongoDB

--- controllers/homePageController.js
+++ controllers/homePageController.js
@@ -1,6 +1,8 @@
+const Blood = require('../models/bloodModels');
+
 const displayHomePage = async (req, res) => {
   try {
-    res.render('home', {authenticated : req.session.authenticated});
+    res.render('home', { authenticated: req.session.authenticated });
   } catch (error) {
     res.status(500).send(error);
   }
@@ -8,25 +10,31 @@ const displayHomePage = async (req, res) => {
 
 const displayVitalsPage = async (req, res) => {
   try {
-      res.render('vitalsPage', {authenticated : req.session.authenticated});
+    res.render('vitalsPage', { authenticated: req.session.authenticated });
   } catch (error) {
-      res.status(400).send(error);
+    res.status(400).send(error);
   }
 };
 
 const displayBodyCompPage = async (req, res) => {
   try {
-      res.render('bodyComposition', {authenticated : req.session.authenticated});
+    res.render('bodyComposition', { authenticated: req.session.authenticated });
   } catch (error) {
-      res.status(400).send(error);
+    res.status(400).send(error);
   }
 };
 
 const displayBloodPage = async (req, res) => {
   try {
-      res.render('bloodPage', {authenticated : req.session.authenticated});
+    const userID = req.session.userID;
+    console.log('UserID:', userID);
+
+    const bloodData = await Blood.findOne({ userID });
+    console.log('Blood Data:', bloodData);
+
+    res.render('bloodPage', { authenticated: req.session.authenticated, bloodData });
   } catch (error) {
-      res.status(400).send(error);
+    res.status(400).send(error);
   }
 };
 
