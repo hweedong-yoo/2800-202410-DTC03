c4f5bd89dedf4601b7d392f39e2b7b409f64feb7
MARIKO HOCKERTZ <mariko.hockertz@gmail.com>
2024-05-23 23:06:39 -0700

    Get userID from session and clean up code

--- controllers/editProfilePageController.js
+++ controllers/editProfilePageController.js
@@ -4,7 +4,7 @@ const BodyComp = require('../models/bodyCompModels');
 const displayPage = async (req, res) => {
   try {
     const userData = await User.findOne({ email: req.session.email });
-    const bodyCompData = await BodyComp.findOne({ userID: userData._id });
+    const bodyCompData = await BodyComp.findOne({ userID: req.session.userID });
 
     const user = {
       dob: userData.dob ? userData.dob.toISOString().substring(0, 10) : "yyyy-mm-dd",
@@ -25,52 +25,32 @@ const displayPage = async (req, res) => {
 const editInformation = async (req, res) => {
   try {
     const { birthday, gender, weight, height } = req.body;
-    const email = req.session.email;
 
     let updateUserData = {};
 
-    if (birthday) {
-      req.session.dob = birthday;
-      updateUserData.dob = birthday;
-    }
-
-    if (gender) {
-      req.session.sex = gender;
-      updateUserData.sex = gender;
-    }
+    if (birthday) updateUserData.dob = birthday;
+    if (gender) updateUserData.sex = gender;
 
     if (Object.keys(updateUserData).length > 0) {
       await User.findOneAndUpdate(
-        { email: email },
+        { email: req.session.email },
         updateUserData
       );
     }
 
 
-    const user = await User.findOne({ email }).select('_id');
-    const userID = user._id;
-    console.log('User _id:', userID);
-
     let updateBodyCompData = {};
 
-    if (weight) {
-      req.session.weight = weight;
-      updateBodyCompData.weight = weight;
-    }
-
-    if (height) {
-      req.session.height = height;
-      updateBodyCompData.height = height;
-    }
-
+    if (weight) updateBodyCompData.weight = weight;
+    if (height) updateBodyCompData.height = height;
+    
     if (Object.keys(updateBodyCompData).length > 0) {
       await BodyComp.findOneAndUpdate(
-        { userID: userID },
+        { userID: req.session.userID },
         updateBodyCompData,
         { upsert: true }
       );
     }
-
 
     res.redirect('/profile')
 
