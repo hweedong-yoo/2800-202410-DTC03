d96c715906c5bef5a4af0ef3e03d8e64e77f5613
Homayoun Khoshi <Khoshihomayoun@gmail.com>
2024-05-29 07:18:18 -0700

    Implement chaining for bodyModelController

--- controllers/bodyModelController.js
+++ controllers/bodyModelController.js
@@ -7,14 +7,11 @@ const displayBodyModelPage = async (req, res) => {
     const userID = req.session.userID;
     console.log(userID);
     const bloodData = await Blood.findOne({ userID });
-    const bloodVulnerability = bloodData.vulnerabilities;
+    const bloodVulnerability = bloodData?.vulnerabilities ?? "--";
     const bodyCompData = await BodyComp.findOne({ userID });
-    const bodyCompVulnerability = bodyCompData.vulnerabilities;
+    const bodyCompVulnerability = bodyCompData?.vulnerabilities ?? "--";
     const vitalsData = await Vitals.findOne({ userID });
-    const vitalsVulnerability = vitalsData.vulnerabilities;
-    console.log(bloodVulnerability);
-    console.log(bodyCompVulnerability);
-    console.log(vitalsVulnerability);
+    const vitalsVulnerability = vitalsData?.vulnerabilities ?? "--";
     res.render('bodyModel', { authenticated: req.session.authenticated , bloodVul: bloodVulnerability , bodyCompVul: bodyCompVulnerability , vitalsVul: vitalsVulnerability});
   } catch (error) {
     res.status(400).send(error);
