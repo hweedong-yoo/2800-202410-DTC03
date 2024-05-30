b49552acdece2ddb163a757da4147fa24cee5e50
WinsonL28 <winsonliang@live.com>
2024-05-30 12:16:25 -0700

    Add fix for more consistent naming

--- controllers/homePageController.js
+++ controllers/homePageController.js
@@ -59,7 +59,7 @@ const displayBodyCompPage = async (req, res) => {
     const bodyCompData = await BodyComp.findOne({ userID: req.session.userID });
     const userData = await User.findOne({ _id: req.session.userID });
 
-    if (!bodyCompData) {
+    if (!bodyCompData.tScore) {
       const newData = new BodyComp({
         userID: req.session.userID,
         tScore: 1,
